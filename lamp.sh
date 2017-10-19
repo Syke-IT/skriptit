72ec279408bf78273dbdd55db552365987583f8a
Syke-IT <32897662+Syke-IT@users.noreply.github.com>
2017-10-19 14:14:54 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-sudo apt install apache2
-sudo apt install mysql-server
-sudo apt install php libapache2-mod-php
-sudo /etc/init.d/apache2 restart
-php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
+# Päivitetään Ubuntun sovellusvalikoima
+sudo apt update
 
+# Asennetaan apache www palvelin
+sudo apt install apache2
+
+# Asennetaan MySQL palvelin
+sudo apt install mysql-server
+
+# Asennetaan PHP
+sudo apt install php libapache2-mod-php
+
+# Sammutetaan apache
+sudo systemctl stop apache2
+
+# Käynnistetään apache, jotta uudet asetukset tulevat voimaan
+sudo systemctl start apache2
+
+# Näytetään apache tila
+sudo systemctl status apache2
+
+# Testaan PHP:n toimivuus
+php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
